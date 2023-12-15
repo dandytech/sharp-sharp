0ca5876641cd359f5209ceac910f344cb58f9128
dandytech <dannkwo@gmail.com>
2023-12-15 12:08:57 +0100

    last commit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 export default function SignUp() {
   return (
-    <div className=" h-[100vh] bg-gradient-to-r from-violet-100 to-blue-100 p-10 sm:mt-0 lg:py-0 lg:pt-0">
+    <div className="h-[90vh] bg-gradient-to-r from-violet-100 to-blue-100 p-10 pt-[100px] lg:py-0 lg:pt-0">
       Sign Up
     </div>
   );
