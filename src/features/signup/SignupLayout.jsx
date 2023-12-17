0b1a70a2c202830fe1daee9935c1cf21131dc884
dandytech <dannkwo@gmail.com>
2023-12-17 11:09:55 +0100

    latest commit

--- src/features/signup/SignupLayout.jsx
+++ src/features/signup/SignupLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import SignupTab from "./signupTab";
+import SignupTab from "./SignupTab";
 import Header from "../../ui/Header";
 import SmallScreenHeader from "../../ui/SmallScreenHeader";
 import Footer from "../../ui/Footer";
