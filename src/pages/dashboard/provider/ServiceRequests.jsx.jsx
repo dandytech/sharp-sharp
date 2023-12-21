05a10f912fb2cc090c82216796deb6997b0c3cef
dandytech <dannkwo@gmail.com>
2023-12-21 12:21:52 +0100

    dashboard commit

--- src/pages/dashboard/provider/ServiceRequests.jsx.jsx
+++ src/pages/dashboard/provider/ServiceRequests.jsx.jsx
@@ -2,7 +2,7 @@
 
 export default function ServiceRequests() {
   return (
-    <div className="right-0 ml-[170px] mt-[80px] h-auto w-[100%] border-2 p-10 shadow-lg">
+    <div className="right-0 ml-[200px] mt-[80px] h-auto w-[100%] border-2 p-10 shadow-lg">
       Available Services
     </div>
   );
