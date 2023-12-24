b5ed7ac98360b5bb4051b0a0daef51a8e0d8bae9
dandytech <dannkwo@gmail.com>
2023-12-24 19:41:32 +0100

    my latest commit

--- src/features/signup/SignupTab.jsx
+++ src/features/signup/SignupTab.jsx
@@ -5,14 +5,14 @@ export default function SignupTab() {
   return (
     <div className="m-auto flex flex-col items-center justify-center  pt-[120px]  shadow-2xl md:w-[80%] lg:w-[50%] lg:pt-10 ">
       <div className="mb-10 text-[24px] text-blue-500 lg:mb-0 lg:mt-20">
-        SELECT ACCOUNT TYPE !!!
+        SELECT ACCOUNT TYPE!
       </div>
 
-      <div className="flex  items-center justify-center gap-10 text-center md:w-[80%] lg:w-[50%] lg:pt-20">
-        <div className="lg:text-[20px] text-[16px]">
+      <div className="flex  items-center justify-center gap-5 text-center md:w-[80%] lg:w-[50%] lg:pt-20">
+        <div className="lg:text-[20px]  text-[16px]">
           <NavLink
             to="/providerReg"
-            className="cursor-pointer rounded-full border-2 border-t-blue-500  bg-gray-100 px-7 py-3 pb-0 hover:font-bold hover:text-blue-500"
+            className="cursor-pointer rounded-full border-2 border-t-blue-500  bg-gray-100 px-3 py-3 pb-0 hover:font-bold hover:text-blue-500"
             activeClassName="active"
           >
             Service Provider
